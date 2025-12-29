c718db79747e87eefb994a1c9a4fdd0cec7a25ac
Qinkai Li <infinityscrub74@gmail.com>
2025-12-29 15:41:39 -0700

    feat: moved the test commandinput logic to ProofCanvas

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,7 @@
 import ProofCanvas from './sections/ProofCanvas'
-import TestCommandInputComponent from './components/TestCommandInput.jsx'
 
 export default function Home() {
   return (
-    <TestCommandInputComponent />
-    //<ProofCanvas />
+    <ProofCanvas />
   );
 }
